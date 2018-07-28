e4c27d22355d20edc9a188c2ef080015397d5b52
kirylmisiuk <kirill.misiuk@gmail.com>
2018-07-28 10:10:21 +0300

    Initial commit

---
+++ app/routes.js
@@ -0,0 +1,76 @@
+
+module.exports = function(app, passport) {
+
+	app.get('/', function(req, res) {
+		res.render('index.ejs');
+	});
+	app.get('/login', function(req, res) {
+	res.render('login.ejs', { message: req.flash('loginMessage') });
+	});
+
+
+	app.post('/login', passport.authenticate('local-login', {
+            successRedirect : '/profile',
+            failureRedirect : '/login',
+            failureFlash : true
+		}),
+        function(req, res) {
+            console.log("hello");
+
+            if (req.body.remember) {
+              req.session.cookie.maxAge = 1000 * 60 * 3;
+            } else {
+              req.session.cookie.expires = false;
+            }
+        res.redirect('/');
+    });
+
+app.get('/signup', function(req, res) {
+		res.render('signup.ejs', { message: req.flash('signupMessage') });
+	});
+
+
+app.post('/signup', passport.authenticate('local-signup', {
+		successRedirect : '/profile',
+		failureRedirect : '/signup',
+		failureFlash : true
+	}));
+
+	app.get('/profile', isLoggedIn, function(req, res) {
+		res.render('profile.ejs', {
+			user : req.user
+		});
+	});
+
+	app.get('/logout', function(req, res) {
+		req.logout();
+		res.redirect('/');
+	});
+
+    app.get('/inbox',function(req,res){
+        res.render('inbox.ejs', {
+            user : req.user
+        });
+    });
+
+
+
+    app.get('/sent',function(req,res){
+        res.render('sent.ejs', {
+            user : req.user
+        });
+    });
+    app.get('/trash',function(req,res){
+        res.render('trash.ejs', {
+            user : req.user
+        });
+    });
+
+};
+
+
+function isLoggedIn(req, res, next) {
+	if (req.isAuthenticated())
+	return next();
+	res.redirect('/');
+}
---
+++ config/database.js
@@ -0,0 +1,10 @@
+// config/database.js
+module.exports = {
+    'connection': {
+        'host': 'localhost',
+        'user': 'kmail',
+        'password': 'kmail228'
+    },
+	'database': 'new',
+    'users_table': 'users'
+};
