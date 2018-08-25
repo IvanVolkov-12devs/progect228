ce384080e89917bedf9c4d2dfb08dcb8d0c3d19d
kirylmisiuk <kirill.misiuk@gmail.com>
2018-08-25 16:02:58 +0300

    commit24

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-var express  = require('express');
-var session  = require('express-session');
+var express = require('express');
+var session = require('express-session');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
-var app      = express();
-var port     = process.env.PORT || 8080;
+var app = express();
+var port = process.env.PORT || 8080;
 var passport = require('passport');
-var flash    = require('connect-flash');
+var flash = require('connect-flash');
 app.use(express.static('./css'));
 require('./scripts/passport')(passport);
 app.use(bodyParser.json());
@@ -30,14 +30,14 @@ app.use(bodyParser.json());
 app.use(morgan('dev'));
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({
-	extended: true
+    extended: true
 }));
 app.set('view engine', 'ejs');
 app.use(session({
-	secret: 'vidyapathaisalwaysrunning',
-	resave: true,
-	saveUninitialized: true
- } )); // session secret
+    secret: 'vidyapathaisalwaysrunning',
+    resave: true,
+    saveUninitialized: true
+})); // session secret
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
